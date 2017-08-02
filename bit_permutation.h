42b6272f7290b810d2433e46e264967d75382f16
Qingchen Yuan <yuanqingchen1994@126.com>
2017-08-02 15:48:39 +0100

    一开始P的数组有误，更改一下

--- bit_permutation.h
+++ bit_permutation.h
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-	unsigned int x=0x00;
+	unsigned int x = 0x00;
 	unsigned short temp[8], i;
 	unsigned int t = 0x00, a[32];
-	unsigned short P[32] = { 1, 9, 17, 25, 5, 13, 21, 29, 2, 10, 18, 26, 6, 14, 22, 30, 3, 11, 19, 27, 7, 15, 23, 31, 4, 12, 20, 28, 8, 16, 24, 32 };
+	unsigned short P[32] = { 0, 8, 16, 24, 4, 12, 20, 28, 1, 9, 17, 25, 5, 13, 21, 29, 2, 10, 18, 26, 6, 14, 22, 30, 3, 11, 19, 27, 7, 15, 23, 31 };
 	//cout << "the size is " << sizeof(unsigned short) << endl; //得到结果是2byte，也就是2x8=16bit
 	temp[0] = 0xf;//0000
 	temp[1] = 0x0;//0001
@@ -28,15 +28,16 @@ int main()
 	{
 		a[i] = (t >> (31 - i)) & 0x1;
 	}
+	cout << P[0] << endl;
 	for (i = 0; i < 32; i++)
 	{
 		x ^= a[P[i]];
 		if (i != 31) x <<= 1;
 	}
-	cout << bitset<32>(t)<<endl;
-	cout << bitset<32>(x);
+	cout <<"t的值是"<< bitset<32>(t) << endl;
+	
 	//cout << "the value of t is "<<t << endl;
-	/*cout << bitset<32>(t)<< endl;
+	cout << "a的值是";
 	cout << a[0];//和matlab不同，数组有a[0]
 	cout << a[1];
 	cout << a[2];
@@ -60,6 +61,15 @@ int main()
 	cout << a[20];
 	cout << a[21];
 	cout << a[22];
-	cout << a[23];*/
+	cout << a[23];
+	cout << a[24];
+	cout << a[25];
+	cout << a[26];
+	cout << a[27];
+	cout << a[28];
+	cout << a[29];
+	cout << a[30];
+	cout << a[31]<<endl;
+	cout << "x的值是" << bitset<32>(x) << endl;
 	return 0;
 }
