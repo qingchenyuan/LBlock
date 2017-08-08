fda1c65e1b9aff9d4542a518eba564243042bb7f
Qingchen Yuan <yuanqingchen1994@126.com>
2017-08-08 11:15:34 +0100

    想要尝试调用函数，然后输入更改mod，然后如果想变量在函数里有效，必须要是全局变量，用static来定义，但是无法让static mod=i,这种做法行不通，失败

--- SDK_init_test.c
+++ SDK_init_test.c
@@ -15,6 +15,7 @@
 #include "xsdps.h"
 #include "ff.h"
 #include "xil_cache.h"
+#include "sleep.h"
 
 XUartPs UartPs;
 XUartPs_Config *UartPs_Config;
@@ -29,7 +30,9 @@ TCHAR *Path = "0:/";
 
 
 void init_LBlock();
-void bram_sd_cotest();
+void file_mangement();
+void read_from_SD();
+void encrypt_decrypt();
 
 //uint64_t mem_arr[1024];
 u64 mem_arr[1024];// long long unsigned int, 64 bit,8 byte,1024行64bit，因为明文是64bit
@@ -43,61 +46,36 @@ char buff1[20];
 char buff2[20];
 unsigned int count1;//used for f_read
 unsigned int count2;//used for f_write
+int mem_count;
 u64 text_in;
+u64 text_out =0;
 int ind;
+static uint16_t a = 0xf;//随便给一些master key的值做测试
+static uint64_t b = 0xf;
+static u32 mod = 1;
 
 int main()
 {
 	init_platform();
+	int i;
+	xil_printf("choose mod: ");
+	scanf("%d",&i);
+	if(i == 1)
+	{
+		static u32 mod =1;
+	}
+	if(i == 0)
+	{
+		static u32 mod =0;
+	}
 	init_LBlock();
-	/**BRAM1_mem = 0x10;
-	*(BRAM1_mem+8)=0x11111111;
-	*BRAM2_mem = 0xffffffff;
-	xil_printf("finished");*/
-	bram_sd_cotest();
-	cleanup_platform();
-	return 0;
-}
-
-void bram_sd_cotest()
-{
-	//open and create file in SD card
-	result = f_open(&file1,"Text.txt",FA_READ);
-	if(result != 0)
-	{
-		xil_printf("ERROR: f_open Text.txt failed, return %d\n",result);
-		return XST_FAILURE;
-	}
-	result = f_open(&file2,"Cipher.txt",FA_CREATE_ALWAYS | FA_WRITE);
-	if(result != 0)
-	{
-		xil_printf("ERROR: f_open Cipher.txt failed, return %d\n",result);
-		return XST_FAILURE;
-	}
-	count1 =8;
-	while(count1 == 8)
-	{
-		result = f_read(&file1,buff1,8,&count1);
-		if(result != 0)
-		{
-			xil_printf("ERROR: f_read Text.txt failed, return %d\n",result);
-			return XST_FAILURE;
-		}
-	    text_in = 0;
-	    xil_printf("the plaintext is ");
-	    for(int i =0; i < count1;i++)
-	    {
-	    	xil_printf("%c",buff1[i]);
-	    	text_in ^= (unsigned int) buff1[i];
-	    	if(i != count1-1) text_in = text_in <<8;
-	    }
-	    xil_printf("\n");
-	    if(count1 != 8) text_in = (text_in <<((8 - count1)*8));
-	    mem_buff[ind] = text_in;
-	    ind++;
-	}
+	file_mangement();
+	read_from_SD();
+	encrypt_decrypt();
 	f_close(&file1);
 	f_close(&file2);
+	cleanup_platform();
+	return 0;
 }
 
 void init_LBlock()
@@ -146,3 +124,225 @@ void init_LBlock()
 		return XST_FAILURE;
 	}
 }
+void file_mangement()
+{
+	//open and create file in SD card
+	if(mod == 1)
+	{
+		result = f_open(&file1,"Text.txt",FA_READ);
+			if(result != 0)
+			{
+				xil_printf("ERROR: f_open Text.txt failed, return %d\n",result);
+				return XST_FAILURE;
+			}
+			result = f_open(&file2,"Cipher.txt",FA_CREATE_ALWAYS | FA_WRITE);
+			if(result != 0)
+			{
+				xil_printf("ERROR: f_open Cipher.txt failed, return %d\n",result);
+				return XST_FAILURE;
+			}
+	}
+	if(mod == 0)
+	{
+		result = f_open(&file2,"Cipher.txt",FA_READ);
+		if(result != 0)
+		{
+			xil_printf("ERROR: f_open Cipher.txt failed, return %d\n",result);
+			return XST_FAILURE;
+		}
+		result = f_open(&file1,"Text.txt",FA_CREATE_ALWAYS | FA_WRITE);
+		if(result != 0)
+		{
+			xil_printf("ERROR: f_open Text.txt failed, return %d\n",result);
+			return XST_FAILURE;
+		}
+	}
+}
+void read_from_SD()
+{
+	//开始从SD卡里读取数据
+	count1 =8;
+	while(count1 == 8)
+	{
+		if(mod == 1)
+		{
+			result = f_read(&file1,buff1,8,&count1);
+			if(result != 0)
+			{
+				xil_printf("ERROR: f_read Text.txt failed, return %d\n",result);
+				return XST_FAILURE;
+			}
+		    text_in = 0;
+		    xil_printf("the plaintext is ");
+		    for(int i =0; i < count1;i++)
+		    {
+		    	xil_printf("%c",buff1[i]);
+		    	text_in ^= (unsigned int) buff1[i];
+		    	if(i != count1-1) text_in = text_in <<8;
+		    }
+
+		    if(count1 != 8) text_in = (text_in <<((8 - count1)*8));
+		    mem_buff[ind] = text_in;
+		    ind++;
+		}
+		if(mod == 0)
+		{
+			result = f_read(&file2,buff1,8,&count1);
+			if(result != 0)
+			{
+				xil_printf("ERROR: f_read Cipher.txt failed, return %d\n",result);
+				return XST_FAILURE;
+			}
+		    text_in = 0;
+		    xil_printf("the ciphertext is ");
+		    for(int i =0; i < count1;i++)
+		    {
+		    	xil_printf("%c",buff1[i]);
+		    	text_in ^= (unsigned int) buff1[i];
+		    	if(i != count1-1) text_in = text_in <<8;
+		    }
+
+		    if(count1 != 8) text_in = (text_in <<((8 - count1)*8));
+		    mem_buff[ind] = text_in;
+		    ind++;
+		}
+		xil_printf("\n");
+	}
+	xil_printf("\n");
+}
+void encrypt_decrypt()
+{
+	//开始吧数据存到BRAM里
+	ind=0;
+	int ind_count = 0;
+	while (mem_buff[ind] != 0)
+	{
+		if(mod == 1)
+		{
+			BRAM1_mem[ind_count] = ((mem_buff[ind]) & 0xffffffff);
+			ind_count++;
+			BRAM1_mem[ind_count] = (((mem_buff[ind]) >> 32) & 0xffffffff);
+			ind_count++;
+			xil_printf("the plaintext stored in BRAM is %x%x\n",BRAM1_mem[ind_count -1],BRAM1_mem[ind_count -2]);
+			ind++;
+			//if BRAM is full or finished
+			if(((ind -1) != 0 && (ind-1) % 1023 == 0) || mem_buff[ind]==0)
+			{
+				if(XCodec_IsIdle(&Codec))
+				{
+					XCodec_Set_a(&Codec, a);
+					XCodec_Set_b(&Codec, b);
+					XCodec_Set_mod_r(&Codec, mod);
+					XCodec_Start(&Codec);
+				}
+				mem_count =0;
+				xil_printf("\n");
+				while(1)
+				{
+					if(XCodec_IsDone(&Codec))
+					{
+						//usleep(36);
+						for(mem_count = 0;mem_count < 2048;mem_count +=2)
+						{
+							if(BRAM2_mem[mem_count + 1] != 0 || BRAM2_mem[mem_count] != 0)
+							{
+								xil_printf("the ciphertext(HEX) are: %x%x\n",BRAM2_mem[mem_count + 1],BRAM2_mem[mem_count]);
+								text_out = BRAM2_mem[mem_count + 1];
+								text_out = text_out << 32;
+								text_out ^= BRAM2_mem[mem_count];
+								if(text_out != 0)
+								{
+									xil_printf("the ciphertext(characters) are: ");
+									for(int i = 0; i<=7;i++)
+									{
+										buff2[i] = text_out >> (56-(8*i)) & 0xff;
+										xil_printf("%c",buff2[i]);
+									}
+									xil_printf("\n");
+									result = f_write(&file2,buff2,8,&count2);
+									if(result != 0)
+									{
+										xil_printf("ERROR: f_write Cipher.txt failed,return %d\n",result);
+									}
+								}
+							}
+						}
+						break;
+					}
+				}
+				for(int i = 0; i < 2048; i++)
+				{
+					BRAM1_mem[i] = 0;
+					BRAM2_mem[i] = 0;
+				}
+				ind_count = 0;
+			}
+		}
+		if(mod == 0)
+		{
+			BRAM1_mem[ind_count] = ((mem_buff[ind]) & 0xffffffff);
+			ind_count++;
+			BRAM1_mem[ind_count] = (((mem_buff[ind]) >> 32) & 0xffffffff);
+			ind_count++;
+			xil_printf("the ciphertext stored in BRAM is %x%x\n",BRAM1_mem[ind_count -1],BRAM1_mem[ind_count -2]);
+			ind++;
+			//if BRAM is full or finished
+			if(((ind -1) != 0 && (ind-1) % 1023 == 0) || mem_buff[ind]==0)
+			{
+				if(XCodec_IsIdle(&Codec))
+				{
+					XCodec_Set_a(&Codec, a);
+					XCodec_Set_b(&Codec, b);
+					XCodec_Set_mod_r(&Codec, mod);
+					XCodec_Start(&Codec);
+				}
+				mem_count =0;
+				xil_printf("\n");
+				while(1)
+				{
+					if(XCodec_IsDone(&Codec))
+					{
+						//usleep(36);
+						for(mem_count = 0;mem_count < 2048;mem_count +=2)
+						{
+							if(BRAM2_mem[mem_count + 1] != 0 || BRAM2_mem[mem_count] != 0)
+							{
+								xil_printf("the plaintext(HEX) are: %x%x\n",BRAM2_mem[mem_count + 1],BRAM2_mem[mem_count]);
+								text_out = BRAM2_mem[mem_count + 1];
+								text_out = text_out << 32;
+								text_out ^= BRAM2_mem[mem_count];
+								if(text_out != 0)
+								{
+									xil_printf("the plaintext(characters) are: ");
+									for(int i = 0; i<=7;i++)
+									{
+										buff2[i] = text_out >> (56-(8*i)) & 0xff;
+										xil_printf("%c",buff2[i]);
+									}
+									xil_printf("\n");
+									result = f_write(&file1,buff2,8,&count2);
+									if(result != 0)
+									{
+										xil_printf("ERROR: f_write Text.txt failed,return %d\n",result);
+									}
+								}
+							}
+						}
+						break;
+					}
+				}
+				for(int i = 0; i < 2048; i++)
+				{
+					BRAM1_mem[i] = 0;
+					BRAM2_mem[i] = 0;
+				}
+				ind_count = 0;
+			}
+		}
+
+	}
+
+
+}
+
+
