e595d1558805b24b0f8a516e1aa60a75c9c8f197
Qingchen Yuan <yuanqingchen1994@126.com>
2017-08-14 19:00:11 +0100

    LBlock_CTR版本，从IP到SDK，运行成功！！！（漏的部分。。。）

---
+++ LBlock_CTR.cpp
@@ -0,0 +1,167 @@
+#include <time.h>
+#include "LBlock.h"
+#include <stdio.h>
+
+using namespace std; //used for the cin/cout/endl etc. commands
+
+void roundkey_less(uint16_t a, uint64_t b,uint32_t rounds,unsigned int rk[32] )
+{ //Key Scheduling
+	unsigned int i;
+	uint16_t temp_a,temp1,temp2;
+	unsigned long long temp_5; //variables used in the function
+	temp1 = (b>>48)&0xFFFF; //take the 16 most significant bits of b
+	rk[0]=(a<<16)^ temp1; //output first round subkey
+	for(i=1;i<rounds;i++)
+	{
+		temp1 = (b>>51) & 0x1FFF; //take the 13 most significant bits of b
+		temp2 = (b>>35) & 0xFFFF; //take the 16 bits after the 13 most significant bits of b
+		temp_a=a;
+		a = a << 29; //shift a by 29 positions to the left
+		b = b << 29; //shift b by 29 positions to the left
+		a = temp2; //implement cyclic shift
+		b^=(temp_a << 13) ^ temp1; //implement cyclic shift
+		temp1=(a>>12)&0xF;//take the 4 bits, corresponding to K79-K76 in the paper
+		temp2=(a>>8)&0xF; //take the 4 bits, corresponding to K75-K72
+		temp1=S[9][temp1];//take the corresponding value from the S-box
+		temp2=S[8][temp2];//take the corresponding value from the S-box
+		a=(temp1<<12)^(temp2<<8)^(a&0x00FF);//update the value of a with the corresponding vallues from the S-boxes at the front 8 bits and the original 8 bits at the back
+		temp_5 = b>>46&0x1F; //take k50-k46 from the masterkey
+		temp_5^=i; //XOR with I
+		b=(b&0xFFF8000000000000)^(temp_5<<46)^(b&0x3FFFFFFFFFFF); //update the value of b
+		temp1 = (b>>48)&0xFFFF;//take the 16 most significant bits of b
+		rk[i]=(a<<16)^ temp1;//Output the leftmost 32 bits of the current master key as a round subkey
+	}
+}
+void roundkey_more(uint64_t c, uint64_t d,uint32_t rounds,unsigned int rk[32] )
+{ //Key Scheduling
+	uint64_t temp_c,temp_d,temp1,temp2,temp3;
+	unsigned int i;
+	rk[0] = c>>32;
+	for(i=1;i<rounds;i++)
+	{
+		//K<<<29
+		temp_c = c>>35;
+		temp_d = d>>29;
+		c = c<<29;
+		d = d>>35;
+		c = c^d;
+		d = (temp_d<<29)^temp_c;
+		//SS替换
+		temp1 = (c>>60)&0xF;
+		temp2 = (c>>56)&0xF;
+		temp1 = S[9][temp1];
+		temp2 = S[8][temp2];
+		c = (temp1<<60)^(temp2<<56)^(c&0xffffffffffffff);
+		// XOR  round counter
+		temp3 = d>>60;
+		temp3 ^= i;
+		d = (temp3<<60)^(d>>4);
+		// left-most 32 bit
+		rk[i] = c>>32;
+	}
+}
+
+unsigned int S_Layer(unsigned int x)
+{
+	unsigned int temp=0x0;
+	int i;
+	for(i=0;i<=7;i++)
+	{
+		temp^=S[7-i][(x>>(28-4*i)&0xF)];//starting from the MSB concatenate the corresponding value from the S-Box at the last 4 bits of temp
+		if (i!=7) temp<<=4; //add four zeroes at the end to support the XOR operation above unless i has reached 7, which signifies the end
+	}
+   // temp^=S[7-i][x&0xF]; //Alternatively use this instead of the if expression at the end of the loop
+	return temp; //return the calculated value
+}
+
+//new bit permutation
+unsigned int P_Layer(unsigned int x)
+{
+	unsigned short temp[32],i;
+	unsigned int t=0x0;
+	for(i=0;i<32;i++)
+	temp[i]=(x>>(31-i))&0x1;
+	for(i=0;i<32;i++)
+	{
+		t^=temp[P[i]];
+		if(i!=31) t<<=1;
+	}
+	return t;
+}
+unsigned int F(unsigned int x, unsigned int k)//Rounding function F
+{
+	x^=k; //X XOR K
+	x=S_Layer(x); //Confusion function with arguement X^K calculated above
+	x=P_Layer(x); //Diffusion function with arguement Confusion function from X^K calculated above
+	return x; //return the calculated value
+}
+void swap(unsigned int *left, unsigned int *right)//A function used to swap the values of the two parts of the plaintext when encrypting; use pointers to reflect the change in the main program
+{
+	unsigned int temp;// temp is used not to lose the value of left
+	temp=(*left); //get the value of left
+	(*left)=(*right); //pass the value of right to left
+	(*right)=temp; //pass the value of temp (left) to right
+}
+
+void codec(uint64_t IV,uint64_t text[1024], uint16_t a, uint64_t b,uint64_t c,uint64_t d,uint32_t keymod,uint32_t rounds, bool mod, uint64_t ans[1024])
+{
+	#pragma HLS interface bram port=ans //a command, which allow the AXI4-Lite (s_axilite) interface to be introduced
+	#pragma HLS RESOURCE variable=ans core=RAM_1P_BRAM
+	#pragma HLS interface bram port=text //a command, which allow the AXI4-Lite (s_axilite) interface to be introduced
+	#pragma HLS RESOURCE variable=text core=RAM_1P_BRAM
+	#pragma HLS interface s_axilite port=return bundle=s_axi //a command, which allow the AXI4-Lite (s_axilite) interface to be introduced
+	#pragma HLS interface s_axilite port=a bundle=s_axi //a command, which allow the AXI4-Lite (s_axilite) interface to be introduced
+	#pragma HLS interface s_axilite port=b bundle=s_axi //a command, which allow the AXI4-Lite (s_axilite) interface to be introduced
+	#pragma HLS interface s_axilite port=mod bundle=s_axi //a command, which allow the AXI4-Lite (s_axilite) interface to be introduced
+ 	#pragma HLS interface s_axilite port=rounds bundle=s_axi
+	#pragma HLS interface s_axilite port=c bundle=s_axi
+	#pragma HLS interface s_axilite port=d bundle=s_axi
+	#pragma HLS interface s_axilite port=keymod bundle=s_axi
+	#pragma HLS interface s_axilite port=IV bundle=s_axi
+
+	unsigned int i; //a simple variable used for control of the loop below
+	unsigned int left, right; //2x32-bit variables used to store the split into two 32-bit parts plain text
+	unsigned int rk1[32]; // an array used to store the roundkey
+	uint64_t I[1024];
+
+	I[0] = IV;
+	for(i=1;i<1024;i++)
+	{
+		I[i]=I[i-1]+1;
+	}
+
+	for(int j=0; j<1024; j++)
+	{
+		if(text[j] !=0 )//1024g counter
+		{
+			left = (I[j]>>32) & 0xFFFFFFFF;
+			right = I[j] & 0xFFFFFFFF;
+			// generate round key according keymod
+			if(keymod == 0)
+			{
+				roundkey_less(a,b,rounds,rk1);
+			}
+			if(keymod == 1)
+			{
+				roundkey_more(c,d,rounds,rk1);
+			}
+			if (mod == 1 || mod == 0)
+			{
+				for(i=0;i<rounds;i++)
+				{
+					right=(right<<20)^(right>>12); //left rotation 20 bit
+					right^=F(left,rk1[i]); //calculate the round function F
+					if (i!=rounds-1) swap(&left,&right); //swap the two parts of the text as in the picture in the paper; pass the addresses of left and right as arguments
+				}
+				ans[j]=left; //store the value of the right part of the ciphertext
+				ans[j]<<=32; //shift it by 32 bits to the left
+				ans[j]^=right; //store the value of the left part at the end of the ciphertext to get the correct answer since the loop above performed one for swap than needed
+				ans[j] = ans[j]^text[j];//XOR
+			}
+
+		}
+
+
+	}
+}
+
---
+++ LBlock_CTR.h
@@ -0,0 +1,33 @@
+#ifndef _lblock_H_
+#define _lblock_H_
+
+#include <iostream> //include a standard library
+#include <stdio.h> //include a standard library
+#include <stdlib.h> //include a standard library
+#include <stdint.h>
+
+using namespace std; //used for the cin/cout/endl etc. commands
+
+const unsigned short S[10][16]={
+							   {14,9,15,0,13,4,10,11,1,2,8,3,7,6,12,5},
+							   {4,11,14,9,15,13,0,10,7,12,5,6,2,8,1,3},
+							   {1,14,7,12,15,13,0,6,11,5,9,3,2,4,8,10},
+							   {7,6,8,11,0,15,3,14,9,10,12,13,5,2,4,1},
+							   {14,5,15,0,7,2,12,13,1,8,4,9,11,10,6,3},
+							   {2,13,11,12,15,14,0,9,7,10,6,3,1,8,4,5},
+							   {11,9,4,14,0,15,10,13,6,12,5,7,3,8,1,2},
+							   {13,10,15,0,14,4,9,11,2,1,8,3,7,5,12,6},
+							   {8,7,14,5,15,13,0,6,11,12,9,10,2,4,1,3}, // The S-box values as presented in the report
+							   {11,5,15,0,7,2,9,13,4,8,1,12,14,10,3,6}};
+const unsigned short P[32] = { 0, 8, 16, 24, 4, 12, 20, 28, 1, 9, 17, 25, 5, 13, 21, 29, 2, 10, 18, 26, 6, 14, 22, 30, 3, 11, 19, 27, 7, 15, 23, 31 };//new bit permutation
+//const unsigned int rounds=32; //number of rounds
+
+void roundkey_less(uint16_t a, uint64_t b,uint32_t rounds,unsigned int rk[32] );
+void roundkey_more(uint64_t c, uint64_t d,uint32_t rounds,unsigned int rk[32] );
+unsigned int S_Layer(unsigned int x);
+unsigned int P_Layer(unsigned int x);
+unsigned int F(unsigned int x, unsigned int k);
+void swap(unsigned int *left, unsigned int *right);
+void codec(uint64_t IV,uint64_t text[1024], uint16_t a, uint64_t b,uint64_t c,uint64_t d,uint32_t keymod,uint32_t rounds, bool mod, uint64_t ans[1024]);
+
+#endif
